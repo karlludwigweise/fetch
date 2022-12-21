011cd8f0c2d769c89de2f9a1e32b896a91640391
Karl Ludwig Weise <office@ludwigweise.de>
2022-12-21 12:32:38 +0100

    feat(delete): Allow json body on delete requests

--- src/index.ts
+++ src/index.ts
@@ -57,11 +57,12 @@ export function put(path: string, body: any = {}, init: RequestInit = {}) {
   return fetch(path, options).then(normalizeResponse).catch(catchResponse);
 }
 
-export function del(path: string, init: RequestInit = {}) {
+export function del(path: string, body: any = {}, init: RequestInit = {}) {
   const options = {
     headers: getHeaders(),
     ...init,
     method: "DELETE",
+    body: JSON.stringify(body),
   };
 
   return fetch(path, options).then(normalizeResponse).catch(catchResponse);
